5003495bb21ca5d647e89a7e77f9aab65c552895
rodolphorocha <rodolpho.r2008@gmail.com>
2026-06-23 09:21:23 -0300

    feat: visualização dos itens da compra na listagem da compra

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,6 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, ItensCompraSerializer 
 from .editora import EditoraSerializer
 from .livro import LivroListRetriveSerializer, LivroListSerializer, LivroSerializer
 from .user import UserRegistrationSerializer, UserSerializer
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -5,7 +5,8 @@ from core.models import Compra
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     status = CharField(source='get_status_display', read_only=True)
-    
+    itens = ItensCompraSerializer(many=True, read_only=True)
+
     class Meta:
         model = Compra
-        fields = ('id', 'usuario', 'status')
+        fields = ('id', 'usuario', 'status', 'itens')
